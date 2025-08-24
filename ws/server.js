891a497a993b062bcce81b46e994400259936899
PhuocThinhkkk <nguyenphuocthinh10c2@gmail.com>
2025-08-24 03:40:17 +0000

    feat: ws server

---
+++ ws/server.js
@@ -0,0 +1,24 @@
+import { Server } from "socket.io";
+import socketHandler from './handler.js';
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3005;
+const DOMAIN = process.env.DOMAIN_CHAT_APP;
+console.log("DOMAIN:", DOMAIN);  
+
+const io = new Server({
+    cors: {
+        origin: DOMAIN,  
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on('connection', (socket) => {
+    socketHandler(socket, io);
+});
+
+io.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
